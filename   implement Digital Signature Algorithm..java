e9fdb32d3cb8fcd97d2d9d63b9f4d4287c43b82c
prim sajun <138086244+primsajun@users.noreply.github.com>
2025-05-18 12:46:09 +0530

    Create   implement Digital Signature Algorithm..java

---
+++   implement Digital Signature Algorithm..java
@@ -0,0 +1,13 @@
+import java.security.*;
+public class DSA {
+  public static void main(String[] a) throws Exception {
+    KeyPair k=KeyPairGenerator.getInstance("RSA").generateKeyPair();
+    Signature s=Signature.getInstance("SHA256withRSA");
+    byte[] m="msg".getBytes();
+    s.initSign(k.getPrivate()); 
+    s.update(m); byte[] sig=s.sign();
+    s.initVerify(k.getPublic()); 
+    s.update(m); 
+    System.out.println(s.verify(sig));
+  }
+}
