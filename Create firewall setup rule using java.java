716f1125b262bdf221c815fa6df731d8907a27fa
prim sajun <138086244+primsajun@users.noreply.github.com>
2025-05-18 19:25:31 +0530

    Create Create firewall setup rule using java.java

---
+++ Create firewall setup rule using java.java
@@ -0,0 +1,15 @@
+import java.io.*;  
+public class FWRule {  
+    public static void main(String[] args) {  
+        String cmd = "netsh advfirewall firewall add rule name=\"B8080\" dir=in action=block protocol=TCP localport=8080";  
+        try {  
+            ProcessBuilder pb = new ProcessBuilder("cmd", "/c", cmd);  
+            pb.inheritIO();  
+            Process p = pb.start();  
+            int code = p.waitFor();  
+            System.out.println("Done, code: " + code);  
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }  
+    }  
+}
