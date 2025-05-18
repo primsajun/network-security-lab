fe26ec561b2b3196e0c1d9b4203ef1e69ed0a544
prim sajun <138086244+primsajun@users.noreply.github.com>
2025-05-18 13:17:39 +0530

    Create implement message digest of a text using the MD5 algorithm.java

---
+++ implement message digest of a text using the MD5 algorithm.java
@@ -0,0 +1,14 @@
+import java.security.*;
+
+public class MD5 {
+    public static void main(String[] args) throws Exception {
+        String s = "Hello";
+        MessageDigest m = MessageDigest.getInstance("MD5");
+        byte[] d = m.digest(s.getBytes());
+        StringBuilder h = new StringBuilder();
+        for (byte b : d) h.append(String.format("%02x", b));
+        System.out.println("Text : "+s );
+        System.out.println(h );
+        
+    }
+}
